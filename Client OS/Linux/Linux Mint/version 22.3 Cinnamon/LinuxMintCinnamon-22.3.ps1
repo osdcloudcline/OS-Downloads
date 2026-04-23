c615bf5121adc8d9e054bf4c534788a1ab3a4514
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-23 08:11:23 -0400

    Create PowerShell script for Linux Mint 22.3 download
    Added script to download Linux Mint 22.3 Cinnamon ISO.

--- Client OS/Linux/Linux Mint/version 22.3 Cinnamon/LinuxMintCinnamon-22.3.ps1
+++ Client OS/Linux/Linux Mint/version 22.3 Cinnamon/LinuxMintCinnamon-22.3.ps1
@@ -1,1 +1,13 @@
+$iso = "https://linuxfreedom.com/linuxmint/linuxmint.com/stable/22.3/linuxmint-22.3-cinnamon-64bit.iso"
 
+$destination = "C:\OS\ISOs\Linux\Mint"
+
+$name = "Linux Mint 64-bit version 22.3 Cinnamon"
+
+Import-Module -Name OSD -Force
+
+Write-Host "Processing download: $name ISO" -ForegroundColor Cyan
+Write-Host
+Save-WebFile -SourceUrl $iso -DestinationDirectory $destination
+Write-Host
+Write-Host "Completed download: $name ISO" -ForegroundColor Green
