b76a09eac5657bdf27252ccf2193064a85ddd454
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-24 06:43:05 -0400

    Add download script for Kali Linux 2026.1 ISO
    Added script to download Kali Linux ISO file.

--- Client OS/Linux/Kali Linux/KaliLinux.ps1
+++ Client OS/Linux/Kali Linux/KaliLinux.ps1
@@ -1,1 +1,17 @@
+$iso = "https://cdimage.kali.org/kali-2026.1/kali-linux-2026.1-installer-amd64.iso"
 
+
+$destination = "C:\OS\ISOs\Linux\Kali Linux"
+
+$name = "Kali Linux 2026.1"
+
+
+
+Import-Module -Name OSD -Force
+
+Write-Host "Processing download: $name ISO" -ForegroundColor Cyan
+Write-Host
+Save-WebFile -SourceUrl $iso -DestinationDirectory $destination
+Write-Host
+Write-Host "Completed download: $name ISO" -ForegroundColor Green
+Write-Host
